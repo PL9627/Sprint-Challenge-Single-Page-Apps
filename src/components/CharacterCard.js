1d19496a893afac67b6d687f077dcef369eab2b8
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-03-20 16:22:38 -0700

    finished with CharacterCard

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,10 +1,12 @@
 import React from "react";
 
-export default function CharacterCard() {
+export default function CharacterCard(props) {
+  const {name, status, image} = props.chara;
   return (
     <div>
-      <p>{}</p>
-      <p>{}</p>
+      <img src = {image} />
+      <p>Name: {name}</p>
+      <p>Status: {status}</p>
     </div>
   );
 }
