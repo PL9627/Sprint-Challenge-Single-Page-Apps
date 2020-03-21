308571eea492b815ef1770d4195b447d6565caee
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-03-20 17:09:40 -0700

    Finished with SearchForm.js and refactored some code

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,11 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
+import axios from 'axios';
 
-export default function SearchForm() {
+const SearchForm = (props) => {
+  const {touched, errors, status} = props;
+
   const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    status && setUsers(status);
+  }, [status]);
  
   return (
     <section className="search-form">
-     // Add a search form here
+      <div>
+        <Form>
+          <Field type = 'text' name = 'search' placeholder = 'Search' />
+              {touched.search && errors.search ? (
+                <span className = 'error'>{errors.search}</span>
+              ) : null};
+          <button type ='submit'>Search</button>
+        </Form>
+      </div>
     </section>
   );
 }
+
+export default withFormik({
+  mapPropsToValues: props => {
+    return {
+      search: props.search || ""
+    };
+  },
+  validationSchema: Yup.object().shape({
+    search: Yup.string().required()
+  }),
+  handleSubmit: (values, {resetForm, setStatus}) => {
+    axios.post("https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/")
+    .then(response => {
+      console.log(response)
+      setStatus(response.data);
+      resetForm();
+    })
+    .catch(err => console.log('Search Error!', err.response))
+  }
+})(SearchForm);
