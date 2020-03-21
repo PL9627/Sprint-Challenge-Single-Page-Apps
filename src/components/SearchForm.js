0db08dae8ea5a4095b7fd36d7e97f45b17961fa4
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-03-20 20:17:40 -0700

    redid SearchForm

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,24 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
-export default function SearchForm({ onSearch }) {
-  const [query, setQuery] = useState({
-    name: ""
-  })
-  const handleInputChange = (event) => {
-    setQuery({ ...query, name: event.target.value })
-  }
+export default function SearchForm() {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const [searchResults, setSearchResults] = useState([]);
+
+  const [data, setData] = useState([]);
+
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+  };
+
+  useEffect(() => {
+    axios.get("https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/")
+    .then(res => {
+      setData(res.data.results)
+    })
+    .catch(err => console.log('ERROR', err));
+
+    const searchItems = data.filter(char => {
+      console.log(char)
+    });
+
+    setSearchResults(searchItems);
+  }, [searchTerm]);
 
   return (
     <section className="search-form">
-      <form onSubmit={() => onSearch(query)}>
-        <input
-          onChange={handleInputChange}
-          placeholder="name"
-          value={query.name}
-          name="name"
-        />
-        <button type="submit">Search</button>
-      </form>
+      <input
+        type = 'text'
+        value = {searchTerm}
+        onChange ={handleChange}
+        placeholder = 'Search'
+         />
+      <button type = "submit">Search</button>
     </section>
   );
 }
