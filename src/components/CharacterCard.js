4206ebd61ecb9e74bb27dcb6d2d684cbce73c4b0
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-03-20 18:59:22 -0700

    styling

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,12 +1,21 @@
 import React from "react";
+import styled from 'styled-components';
 
+const Card = styled.div`
+  width: 30%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 1px solid black;
+  margin: 2% 0 2% 35%;
+`
 export default function CharacterCard(props) {
   return (
     <div>
-      <div>
+      <Card>
         <p>Name: {props.name}</p>
         <p>Status: {props.status}</p>
-      </div>
+      </Card>
     </div>
   );
 }
