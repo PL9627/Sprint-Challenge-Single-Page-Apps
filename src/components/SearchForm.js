392fe5ac5f0a97df33152bd2832972bbea828333
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-03-20 18:32:32 -0700

    Answered README and still working on search form

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,33 @@
 import React, { useState } from "react";
 
-export default function SearchForm() {
- 
+export default function SearchForm(props) {
+  const charas = props.charaList;
+
+  const [searchName, setSearchName] = useState({
+    name: ''
+  })
+
+  const handleChange = (event) => {
+    setSearchName({...searchName, name: event.target.value})
+  }
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    props.setCharaList(charas.filter(characters => characters.name === searchName))
+  }
+
   return (
     <section className="search-form">
-     // Add a search form here
+     <form onSubmit = {submitHandler}>
+      <input 
+        type = 'text'
+        name = 'name'
+        placeholder = 'Search Characters'
+        onChange = {handleChange}
+        value = {searchName.name} />
+      <button type = 'submit'>Search</button>
+     </form>
     </section>
   );
 }
